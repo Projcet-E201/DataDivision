b3fbec0e39ab79344455b90f5102eff5fc30cd93
mauv2sky <mauv2sky@gmail.com>
2023-04-19 17:48:55 +0900

    Feat. Kafka Consumer config

--- src/main/java/com/example/data/DataApplication.java
+++ src/main/java/com/example/data/DataApplication.java
@@ -7,9 +7,11 @@ import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.kafka.annotation.EnableKafka;
 
 // ./gradlew clean build -Pprofiles="prod,secret"
 
+@EnableKafka
 @SpringBootApplication
 @EnableJpaAuditing
 public class DataApplication {
