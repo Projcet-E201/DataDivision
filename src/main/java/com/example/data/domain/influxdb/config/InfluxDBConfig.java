b317cb73006f6653ea0a4863362f7f849e966cac
goeom77 <7eom14@gmail.com>
2023-04-25 11:56:42 +0900

    Feat InfluxDB 데이터 저장

--- src/main/java/com/example/data/domain/influxdb/config/InfluxDBConfig.java
+++ src/main/java/com/example/data/domain/influxdb/config/InfluxDBConfig.java
@@ -6,42 +6,28 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
 public class InfluxDBConfig {
 
-    @Value("${influxdb.url}")
+    @Value("${spring.influxdb.url}")
     private String url;
 
-    @Value("${influxdb.username}")
+    @Value("${spring.influxdb.username}")
     private String username;
 
-    @Value("${influxdb.password}")
+    @Value("${spring.influxdb.password}")
     private String password;
 
-    @Value("${spring.influx.token}")
-    private char[] token;
+    @Value("${spring.influxdb.token}")
+    private String token;
 
-    @Value("${spring.influx.org}")
-    private String org;
-
-    @Value("${spring.influx.bucket}")
-    private String[] bucket;
 
     @Bean
     public InfluxDBClient influxDBClient() {
-        return InfluxDBClientFactory.create(url, token, org, bucket[0]);
+        return InfluxDBClientFactory.create(url, token.toCharArray());
     }
-    @Bean
-    public InfluxDBClient influxDBClientDay() {
-        return InfluxDBClientFactory.create(url, token, org, bucket[1]);
-    }
-    @Bean
-    public InfluxDBClient influxDBClientWeek() {
-        return InfluxDBClientFactory.create(url, token, org, bucket[2]);
-    }
-    @Bean
-    public InfluxDBClient influxDBClientMonth() {
-        return InfluxDBClientFactory.create(url, token, org, bucket[3]);
-    }
+
 
 }
