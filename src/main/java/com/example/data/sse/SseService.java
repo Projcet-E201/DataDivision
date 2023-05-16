ea2c2dd1f5a02df7dea142e3f21916c3ff2acf49
mauv2sky <mauv2sky@gmail.com>
2023-05-16 14:03:58 +0900

    Feat. SSE Bean 생성

--- src/main/java/com/example/data/sse/SseService.java
+++ src/main/java/com/example/data/sse/SseService.java
@@ -1,5 +1,6 @@
 package com.example.data.sse;
 
+import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.io.IOException;
@@ -7,6 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
+@Service
 public class SseService {
     private final List<SseEmitter> emitters = new CopyOnWriteArrayList<>();
 
